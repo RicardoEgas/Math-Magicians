b56818eb3d3a50b777ccf0b531e3368d093244a8
kcccendana <kc_telyn@yahoo.com>
2023-08-17 02:46:11 +0900

    add additional tests for calculate.js

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,4 +1,4 @@
-import calculate from './calculate';
+import calculate from "./calculate";
 
 describe('calculate data', () => {
   test('test if AC is pressed', () => {
@@ -7,8 +7,8 @@ describe('calculate data', () => {
   });
 
   test('test if another number is added', () => {
-    const result = calculate({ total: 2, next: 5, operation: '+' }, '0');
-    expect(result).toEqual({ total: 2, next: '50', operation: '+' });
+    const result = calculate({ total: 2, next: 0, operation: '+' }, '0');
+    expect(result).toEqual({ total: 2, next: '0', operation: '+' });
   });
 
   test('test if another number is added', () => {
@@ -21,8 +21,24 @@ describe('calculate data', () => {
     expect(result).toEqual({ total: '2', next: '5.', operation: '+' });
   });
 
-  test('test equal sign', () => {
+  test('perform valid operation', () => {
     const result = calculate({ total: '2', next: '5', operation: '+' }, '=');
     expect(result).toEqual({ total: '7', next: null, operation: null });
+  });
+  test("if buttonName is '.'", () => {
+    const result = calculate({ total: '2', next: '5.', operation: '+' }, '.');
+    expect(result).toEqual({ total: '2', next: '5.', operation: '+' });
+  });
+  test("Set number to positive/negative when '-/+' is pressed", () => {
+    const result = calculate({ total: '2', next: '5', operation: '+' }, '+/-');
+    expect(result).toEqual({ total: '2', next: '-5', operation: '+' });
+  });
+  test("save the final operation", () => {
+    const result = calculate({ total: '2', next: null, operation: '+' }, 'x');
+    expect(result).toEqual({ total: '2', next: null, operation: 'x' });
+  });
+  test("save operation", () => {
+    const result = calculate({ total: '2', next: null, operation: null }, 'x');
+    expect(result).toEqual({ total: '2', next: null, operation: 'x' });
   });
 });
